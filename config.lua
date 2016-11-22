a26b4df0d3873e4d9a232bfbdd48dee934993492
Valentin Granados <valefcfhc@gmail.com>
2016-11-22 12:58:42 -0600

    config

--- config.lua
+++ config.lua
@@ -1,10 +1,36 @@
-application =
-{
+if ( string.sub( system.getInfo("model"), 1, 4 ) == "iPad" ) then
+
+	print("ipad")
+
+   application =
+   {
+      content =
+      {
+         width = 660,
+         height = 1024,
+         scale = "zoomStretch",
+         xAlign = "center",
+         yAlign = "center",
+         imageSuffix =
+         {
+            ["@2x"] = 1.5,
+            ["@4x"] = 3.0,
+         },
+      },
+   }
+else 
+	application =
+	{
         content =
         {	
-            width = 576,
+            width = 600,
             height = 1024,
-            scale = "letterbox",
+            scale = "zoomStretch",
             fps = 35,
         },
-}
+	}
+end
+
+
+
+
